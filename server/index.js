fccc0aeba0cc723e1020a3f56e2eb59eb38987ba
PrasanthRajuNPR <prasanthrajunidimoru@gmail.com>
2026-01-19 00:54:05 +0530

    Fix cloudConnect reference

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const cloudConnect=require("./config/cloudinary")
 const userRouter = require("./routes/User");
 const ProfileRouter = require("./routes/Profile");
 const courseRouter = require("./routes/Course");
