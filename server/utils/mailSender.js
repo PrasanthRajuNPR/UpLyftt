9e391600d6f3623a1a84126f7deac8384d38ca17
PrasanthRajuNPR <prasanthrajunidimoru@gmail.com>
2026-01-19 04:33:48 +0530

    Fix cloudConnect reference

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -11,7 +11,7 @@ const mailSender = async (email, title, body) => {
         pass: process.env.MAIL_PASS,
       },
       // ADD THIS: Increase timeout settings for slow server connections
-      connectionTimeout: 20000, // 10 seconds
+      connectionTimeout: 100000, // 10 seconds
       greetingTimeout: 10000,
     });
 
