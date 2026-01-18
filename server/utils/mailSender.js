af4fda0044a4ba17d150fa9d0c36484138de9160
PrasanthRajuNPR <prasanthrajunidimoru@gmail.com>
2026-01-19 04:55:11 +0530

    Fix cloudConnect reference

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,9 +4,7 @@ const mailSender = async (email,title,body)=>{
     try{
         console.log(email);
         const transporter = nodemailer.createTransport({
-            host:process.env.MAIL_HOST,
-                  port: 587, // Use 587 for STARTTLS
-
+            service: "gmail",
             auth:{
                 user:process.env.MAIL_USER,
                 pass:process.env.MAIL_PASS,
